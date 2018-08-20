74278cc3a234013a79ad5631d14bc81dfaf16fc7
gigo123 <gigo123black@gmail.com>
2018-08-20 20:10:26 +0300

    cinecamera   начало
    добавление второй камери

--- Assets/girl/girl_start.cs
+++ Assets/girl/girl_start.cs
@@ -3,49 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class girl_start : MonoBehaviour {
-    //IEnumerator Example()
-    //
-    public GameObject start_portal;
-    Material portal_material;
-    float opaciry_val = 0;
-    float fade = 0;
-    float moveOfsetx = 0;
-    void Start () {
-        //   sr= gameObject.AddComponent<girl_start>;
-        this.transform.position = new Vector3(26, -10, 2);
-        //ExecuteAfterTime(5);
-          //  this.transform.position = new Vector3(26, 0, 0);
-
-        }
-
+   
+    public GameObject start_portal;   //  оюек портала
+    Material portal_material; //  материал портала
+    float opaciry_val = 0.1f; //  сначене прозначности портала
+    float fade = 0;//  затухание протала
+    float moveOfsetx = 0;//  модификатор движения 
+    bool isTriger = false;
+    void Start () {     
+       this.transform.position = new Vector3(27, -10, -7);   //  стартова позицыя 
+        portal_material = start_portal.GetComponent<Renderer>().sharedMaterial;
+        portal_material.SetFloat("_fade", fade);
+        portal_material.SetFloat("_opacity", opaciry_val);
+    }
     // Update is called once per frame
     void Update()
     {
-
-        bool isTriger = false;
         bool portal_destroy = false;
-        if (start_portal)
+        if (start_portal)  // якщо портал ще ынуэ
         { 
-            portal_material = start_portal.GetComponent<Renderer>().sharedMaterial;
             portal_material.SetFloat("_fade", fade);
         }
         if (Time.timeSinceLevelLoad > 5.2f)
         {
             if (isTriger == false)
             {
-                this.transform.position = new Vector3(26, 0.8f, 2);
+                this.transform.position = new Vector3(27, 0.8f, -7);
                 isTriger = true;
                 
             }
-
         }
         else
         {
             fade += 0.005f;
         }
         if (Time.timeSinceLevelLoad > 6.2f)
-        {
-        
+        {  
             if (start_portal)
             {
                 portal_material.SetFloat("_opacity", opaciry_val);
@@ -64,10 +57,8 @@ public class girl_start : MonoBehaviour {
         }
         if (Time.timeSinceLevelLoad > 18.5f)
         {
-            transform.position = new Vector3(26 + moveOfsetx, 0.8f, 2+ moveOfsetx);
-             moveOfsetx += 0.1f;
+         //   transform.position = new Vector3(26 + moveOfsetx, 0.8f, 2+ moveOfsetx);
+          //   moveOfsetx += 0.1f;
         }
-
-    }
-        
+    }      
 }
